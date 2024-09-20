12a47dc95d64b5db7497f269b6408110af9572d8
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-09-21 05:12:08 +0530

    Bug Fix 2

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
-import Input from "./Input";
+
 import RTE from "./RTE";
 import Select from "./Select";
 import Signup from "./Signup";
@@ -12,8 +12,7 @@ import Login from "./Login";
 import PostForm from './post-form/PostForm'
 import PostCard from './PostCard'
 import AuthLayout from './AuthLayout'
-
-
+import Input from "./input";
 export {Header,Footer,Container,Logo,LogoutBtn,
     Button,Input,RTE,Select,Signup, Login,
     PostForm,
